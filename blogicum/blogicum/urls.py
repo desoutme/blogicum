bcd74411416c7dca269a2c2a5ac71f63dde89be0
Denis Polyakov <desoutme@vk.com>
2025-07-05 02:02:08 +0300

    Создана страница регистрации auth/registration/

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 
 handler403 = 'core.views.csrf_failure'
@@ -12,7 +14,15 @@ urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
     path('pages/', include('pages.urls', namespace='pages')),
     path('admin/', admin.site.urls),
-    path("auth/", include('django.contrib.auth.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog:index'),
+        ),
+        name="registration")
 ]
 
 if settings.DEBUG:
