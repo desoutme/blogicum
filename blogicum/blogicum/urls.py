0021257d622be1cc1d99481b4a24ecb3f5b95b5d
Denis Polyakov <desoutme@vk.com>
2025-07-05 01:54:04 +0300

    Подключите к проекту пути для работы с пользователями из django.contrib.auth.urls.

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
     path('pages/', include('pages.urls', namespace='pages')),
     path('admin/', admin.site.urls),
+    path("auth/", include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
